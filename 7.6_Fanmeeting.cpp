0a2f695613601e088c50a5350eca35a0940d1033
achievermina <lmina820@gmail.com>
2019-04-23 10:51:24 -0400

    working on Karatsuba

---
+++ 7.6_Fanmeeting.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+vector<int> karatsuba(const vector<int>& a, const vector<int>& b){
+    int an = a.size();
+    int bn= b.size();
+
+    if(an<bn)
+        retrun karatsuba(b,a);
+
+    //base case
+    if(an == 0|| bn==0)
+        return vecotr<int>();
+
+
+
+    int half = an/2;
+
+
+    vector<int> a0(a.begin(), a.begin()+half);
+    vector<int> a1(a.begin()+half, a.end());
+    vecotr<int> b0(b.begin(), b.begin()+min<int>(b.size(0,half)));
+    vector<int> b1(b.begin()++min<int>(b.size(0,half), b.end());
+
+
+    vector<int> z1, z2, z0;
+
+    z2 = karatsuba(a1, b1);
+    z0 = karatsuba(a0,b0);
+
+
+
+}
