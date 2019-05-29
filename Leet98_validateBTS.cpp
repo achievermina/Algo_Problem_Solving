921c24acf134b924eec336df46a64bb028a8b0b3
achievermina <lmina820@gmail.com>
2019-05-29 10:18:08 -0400

    update the code

--- Leet98_validateBTS.cpp
+++ Leet98_validateBTS.cpp
@@ -1,7 +1,41 @@
+
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        
+        return recursiveCheck(root, NULL, NULL);
+    }
+    
+    bool recursiveCheck(TreeNode* node, int min, int max){
+        if(node == NULL)
+            return true;
+        
+        if( min !=NULL &&  node->val <= min )
+            return false;
+        if( max !=NULL && node->val >= max  )
+            return false;
+        
+        
+        return recursiveCheck(node->left, min, node->val) && recursiveCheck(node->right, node->val, max);
+    }
+};
+
+
+
+
+
+
 /*
 
 Leetcode 98.Validate Binary Search Tree
 Runtime error
+
+-- missed the case of 
+    5
+ 1.     6
+       4. 7
+4<5 therefore, it should be on the left.
 
 */
 
