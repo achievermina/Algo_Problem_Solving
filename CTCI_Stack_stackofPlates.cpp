24a795cae9f025af46b5a5371ed58d6aaf42eb15
achievermina <lmina820@gmail.com>
2019-06-16 11:29:02 -0400

    add Class

--- CTCI_Stack_stackofPlates.cpp
+++ CTCI_Stack_stackofPlates.cpp
@@ -4,46 +4,103 @@
 //
 //  Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
 //composed of several stacks and should create a new stack once the previous one exceeds capacity.
-//Implement a data structure SetOfStacks that mimics this. SetO-fStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
+//Implement a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
 //SetOfStacks.push() and SetOfStacks. pop() should behave identically to a single stack (that is, pop() should return the same values as it would if there were just a single stack).
 //FOLLOW UP
-//ImplementafunctionpopAt(int index)whichperformsapopoperationonaspecificsub-stack.
+//Implement a function popAt(int index)whichperformsapopoperationonaspecificsub-stack.
 //
 
 
 //stack syntax
 #include <iostream>
-#include <string>
+#include<vector>
 #include <stack>
 using namespace std;
 
-DEFINE SIZE = 10;
+#define STACKMAX 5
+#define SETSIZE 100
 
-class Stack{
-    int count;
+class SetOfStacks{
+public:
+    int countElements;
+    int stackIdx;
+    //vector of pointers
+    vector<stack<int>*> stackArr;
     
-    Stack(){
-        
+    SetOfStacks(){
+        stackIdx =0;
+        countElements =0;
+        stack<int> s;
+        (stackArr).push_back(&s);
     }
     
-    
-    bool checkSize(){
-        if(count>=SIZE){
+    bool checkEnoughSize(stack<int> currentStack){
+        if(currentStack.size()>= STACKMAX){
             return false;
         }
         return true;
-        
     }
     
-    int popAt(int index){
+    void push(int value){
+        stack<int> currentStack = *stackArr[stackIdx];
         
+        if(checkEnoughSize(currentStack)){
+            (*(stackArr[stackIdx])).push(value);
+        }else{
+            stack<int> sNew = createNewStack();
+            sNew.push(value);
+        }
+        countElements++;
+        cout<<"pushed value of "<<value<<endl;
+        cout<<"stack number "<<stackIdx+1<<endl;
     }
     
-    int push(){
-        if(checkSize()){
-            
+    stack<int> createNewStack(){
+        stack<int> newS;
+        stackIdx++;
+        stackArr[stackIdx] = &newS;
+        countElements =0;
+        
+        return newS;
+    }
+    
+    void pop(){
+        stack<int> currentStack = *stackArr[stackIdx];
+
+        if(currentStack.empty()){
+            stackIdx --;
+            currentStack = *stackArr[stackIdx];
+            cout<<"popped value of "<<currentStack.top()<<endl;
+            currentStack.pop();
+        }else{
+            cout<<"popped value of "<<currentStack.top()<<endl;
+            currentStack.pop();
         }
         
+        cout<<"stack number "<<stackIdx+1<<endl;
     }
+    
+    
+//    int popAt(int index){
+//        pass;
+//    }
+    
+
 };
+
+int main(){
+    SetOfStacks haha;
+    haha.push(3);
+    haha.push(4);
+    haha.push(5);
+    haha.push(6);
+    haha.push(7);
+    haha.push(8);
+    haha.push(9);
+    haha.push(10);
+
+    haha.pop();
+    
+    
+}
 
