ba0945d1ece87b082d5956c91446989f588db660
achievermina <lmina820@gmail.com>
2019-08-04 13:07:03 -0400

    coin change DP update

--- Leet322_CoinChange.cpp
+++ Leet322_CoinChange.cpp
@@ -10,19 +10,40 @@ Dynamic programming
 //using Recursive call
 
 
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+int coinChange(vector<int>& coins, int amount){
+    
+    int mem[amount+1];
+    mem[0]=0;
+    
+    sort(coins.begin(), coins.end());
+    
+    for(int currentAmount =1; currentAmount<=amount; currentAmount++){
+        mem[currentAmount] = 1000000000;
+        for(int i=0; i< coins.size(); ++i){
+            if(currentAmount>= coins[i] && mem[currentAmount-coins[i]]!=-1){
+                mem[currentAmount] = min( mem[currentAmount-coins[i]]+1, mem[currentAmount]);
+            }
+        }
+        if(mem[currentAmount] == 1000000000)
+            mem[currentAmount] =-1;
     }
-
-    int recursiveCoinChange(){
-
-    }
-};
+    return mem[amount];
+}
 
 
-
-//Dynamic Programming
-
-
+int main(){
+    vector<int> eg = {2};
+    int amount =3;
+    
+    cout<<coinChange(eg, amount);
+    
+    
+}
